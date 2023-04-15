57b5e6a2dacf2ca61ba74eb1fdd7cf3d449c9496
Sardor <cr720051010@gmail.com>
2023-04-15 23:18:15 +0300

    input field state add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import InputField from "./components/InputField/InputField";
+import {Todo} from "./model";
 
 const App: React.FC = () => {
+    const [todo, setTodo] = useState<string>("");
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const handleAdd = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (todo) {
+            setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}])
+            setTodo("")
+        }
+    }
+
     return (
         <div className="App">
-          <span className="heading">Taskify</span>
-            <InputField />
+            <span className="heading">Taskify</span>
+            <InputField
+                todo={todo}
+                setTodo={setTodo}
+                handleAdd={handleAdd}
+            />
         </div>
     )
 }
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import './Style.css'
 
-function InputField() {
+interface Props {
+    todo: string,
+    setTodo: React.Dispatch<React.SetStateAction<string>>,
+    handleAdd: (e:React.FormEvent) => void;
+}
+
+function InputField({todo, setTodo, handleAdd}: Props) {
     return (
-        <form className="input">
-            <input type="input" placeholder="Enter a task" className="input_box"/>
-            <button className="input_submit" type="submit">Go</button>
+        <form className="input" onSubmit={handleAdd}>
+            <input
+                type="input"
+                placeholder="Enter a task"
+                className="input_box"
+                value={todo}
+                onChange={event => setTodo(event.target.value)}
+            />
+            <button
+                className="input_submit"
+                type="submit"
+            >Go</button>
         </form>
     )
 }
