6fec5ea9edd5535e5ae69661afd94676e6f3bbca
Sardor <cr720051010@gmail.com>
2023-04-15 21:27:37 +0300

    initial files with JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import React, { createElement as e, useState } from 'react'
+import React from 'react'
 
 function App() {
+  return (
+    <div className='container mx-auto max-w-2xl pt-5'>
 
-  const [count, setCount] = useState(0);
-
-  return e('div', { className: 'wrapper' }, [
-    e('h1', { className: 'heading', key: 1 }, `Hello From JSX ${count}`),
-    e('button', {
-      className: 'py-2 px-4 mt-4 border',
-      key: 2,
-      onClick: () => setCount(count + 1),
-    }, 'Click me!!')
-  ])
+    </div>
+  )
 }
 
 export default App;
